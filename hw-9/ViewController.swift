9af796f41ce9998f03e8e07d3e2c00b7a8bf3fb5
quanyshev <kuanyshev.asylbek@gmail.com>
2024-03-06 17:25:04 +0500

    Cleared.

--- hw-9/ViewController.swift
+++ hw-9/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  hw-9
-//
-//  Created by Bek Q on 06.03.2024.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
 
